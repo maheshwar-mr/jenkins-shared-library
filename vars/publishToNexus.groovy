465364d30b3b2910656f4385d98d5de7481e029a
Maheshwar Reddy <37240803+maheshwar807@users.noreply.github.com>
2020-07-28 16:43:57 +0530

    Update publishToNexus.groovy

--- vars/publishToNexus.groovy
+++ vars/publishToNexus.groovy
@@ -1,6 +1,6 @@
 def call(String artifactPath, String repositoryURL){
 	withCredentials([usernamePassword(credentialsId: 'nexus-user-credentials', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-		sh "curl -v -u $userId:$pass -F "maven2.generate-pom=false" -F "maven2.asset1=@pom.xml" -F "maven2.asset1.extension=pom" -F "maven2.asset2=$artifactPath" -F "maven2.asset2.extension=jar" $repositoryURL"
+		sh "curl -v -u $userId:$pass -F maven2.generate-pom=false -F maven2.asset1=@pom.xml -F maven2.asset1.extension=pom -F maven2.asset2=$artifactPath -F maven2.asset2.extension=jar $repositoryURL"
         }               
 }
 
